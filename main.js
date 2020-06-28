df2ede1ebe2049ee29b5dcf367b4bfd6bc838fb5
Pat Mulhern <patrick.mulhern80@gmail.com>
2020-06-28 12:08:10 -0400

    Adding  5 day forecast base

--- main.js
+++ main.js
@@ -16,33 +16,14 @@ function uvColor() {
         uv.addClass("severe").removeClass("low moderate high");
     }
 };
-    
-    //   // update current, past, future hour with style 
-    //   if(currentRowHour == currentHour) {
-    //     //present 
-    //     currentRow.addClass("present").removeClass("past future");
-    //   } else if (currentRowHour > currentHour) {
-    //     // future
-    //     currentRow.addClass("future").removeClass("past present");
-    //   }
-    //     else {
-    //     // past
-    //     currentRow.addClass("past").removeClass("future present");
-    //     }
-   
-
-
-
 
 $("#searchBtn").on("click", function(event) {
-    event.preventDefault();
+    event.preventDefault();    
 
     var city = $("#citySearch").val();
     console.log(city)
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=cd805afbcca4574c470ae7a3e6fa7c0b&units=imperial";
-    var queryURL2 = "https://api.opencagedata.com/geocode/v1/json?q=" + city + "&key=35628c0d9dc346a78ee4fc254dfa24b1"
-    
 
     $.ajax({
         url: queryURL,
@@ -54,53 +35,59 @@ $("#searchBtn").on("click", function(event) {
         var humidity = response.main.humidity
         var windSpeed = response.wind.speed
         var date = moment().format('M/DD/YYYY');
+        var lat = response.coord.lat
+        var long = response.coord.lon
 
         console.log(city)
         console.log(temp)
         console.log(humidity)
         console.log(windSpeed)
-
-        $("#city").append(city +" (" + date + ")");
-        $("#temp").append(temp + String.fromCharCode(176) + "F");
-        $("#humidity").append(humidity + String.fromCharCode(37));
-        $("#windspeed").append(windSpeed + " MPH");
+        console.log(lat)
+        console.log(long)
+        
+        $("#city").empty().append(city +" (" + date + ")");
+        $("#temp").empty().append(temp + String.fromCharCode(176) + "F");
+        $("#humidity").empty().append(humidity + String.fromCharCode(37));
+        $("#windspeed").empty().append(windSpeed + " MPH");
         $("#weatherImage").attr({"src": "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png",
         "height": "100px", "width":"100px"});
     });
+     
+    var lat = localStorage.getItem("lattitude", lat)
+    var long = localStorage.getItem("longitude", long)
+    console.log(lat)
+    console.log(long)
 
-        // 5 Day Forecast Query
+    var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + long +"&exclude=hourly,minutely&appid=cd805afbcca4574c470ae7a3e6fa7c0b&units=imperial";
     $.ajax({
         url: queryURL2,
         method: "GET"
     }).then(function(response2) {
         console.log(response2)
-        var lat = response2.results[0].geometry.lat
-        var long = response2.results[0].geometry.lng
-        console.log(lat)
-        console.log(long)
-        localStorage.setItem("lattitude", lat)
-        localStorage.setItem("longitude", long)
-    });
-    var lat = localStorage.getItem("lattitude", lat)
-    var long = localStorage.getItem("longitude", lat)
-    console.log(lat)
-    console.log(long)
-
-    var queryURL3 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + long +"&exclude=daily,hourly,minutely&appid=cd805afbcca4574c470ae7a3e6fa7c0b&units=imperial";
-    $.ajax({
-        url: queryURL3,
-        method: "GET"
-    }).then(function(response3) {
-        console.log(response3)
     
-    var uvIndex = response3.current.uvi
+    var uvIndex = response2.current.uvi
     console.log(uvIndex)
-    
-    // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-    $("#uvIndex").append(uvIndex);
     localStorage.setItem("uvScore", uvIndex)
-
+    $("#uvIndex").empty().append(uvIndex);
     uvColor();
-    });
+
+    for (let i = 0; i < 5; i++) {
+        
+        var temp = response2.daily[i].temp.day;
+        var humidity = response2.daily[i].humidity;
+        var weatherIcon = response2.daily[i].weather[0].icon
+
+        $(`#weatherImage${i}`).attr({"src": "http://openweathermap.org/img/w/" + weatherIcon + ".png", "height": "50px", "width":"50px"});
+        $(`#Temp${i}`).empty().append(temp + String.fromCharCode(176) + "F");
+        $(`#Humidity${i}`).empty().append(humidity + String.fromCharCode(37));
+    }
+});
 });
 
+let clearBtn = document.getElementById("clearBtn");
+
+clearBtn.addEventListener("click",function() {
+    window.localStorage.clear();
+    location.reload();
+    alert("Your tasks have been cleared")
+})
