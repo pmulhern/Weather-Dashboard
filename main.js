0a1365e8e0a785102e44f637022eb162573e7e31
Pat Mulhern <patrick.mulhern80@gmail.com>
2020-06-28 15:47:53 -0400

    Corrected Local Storage Issue with UV Index, added Local Storage for City Searches & corrected timing of Lat/ Long variables

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
 var city = "";
 var date = moment().format('dddd, MMMM Do');
-var uvScore = localStorage.getItem("uvScore")
-var uv = $(".uv")
+var uv = $(".uv");
 
-console.log(uvScore)
-
-function uvColor() {
-    if (uvScore <= 2){
-        uv.addClass("low").removeClass("moderate high severe");
-    } else if(uvScore > 2 && uvScore <= 5.99 ) {
-        uv.addClass("moderate").removeClass("low high severe");
-    } else if(uvScore >= 6 && uvScore <= 6.99 ) {
-        uv.addClass("high").removeClass("low moderate severe");
-    } else {
-        uv.addClass("severe").removeClass("low moderate high");
-    }
-};
 
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();    
@@ -51,7 +37,37 @@ $("#searchBtn").on("click", function(event) {
         $("#windspeed").empty().append(windSpeed + " MPH");
         $("#weatherImage").attr({"src": "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png",
         "height": "100px", "width":"100px"});
+
+        localStorage.setItem("lattitude", lat)
+        localStorage.setItem("longitude", long)
+
+        let storedCity = []
+        // storedCity.push(city)
+        // console.log(storedCity)
+        
+        if(localStorage.getItem("City") === null ) 
+        {
+        let storedCity = [];
+        storedCity.push(city);
+        
+        localStorage.setItem("City", storedCity)}
+        else {
+        let tempStoredCity = localStorage.getItem("City");
+
+        storedCity.push(city)
+        storedCity.push(tempStoredCity)
+
+        localStorage.setItem("City", storedCity)
+
+        } 
+       
+
     });
+
+    var timeDelay = 1000;           // MILLISECONDS (5 SECONDS).
+    setTimeout(loadJSON, timeDelay);  // MAKE THE AJAX CALL AFTER A FEW SECONDS DELAY.
+
+    function loadJSON() {
      
     var lat = localStorage.getItem("lattitude", lat)
     var long = localStorage.getItem("longitude", long)
@@ -61,7 +77,7 @@ $("#searchBtn").on("click", function(event) {
     var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + long +"&exclude=hourly,minutely&appid=cd805afbcca4574c470ae7a3e6fa7c0b&units=imperial";
     $.ajax({
         url: queryURL2,
-        method: "GET"
+        method: "GET",
     }).then(function(response2) {
         console.log(response2)
     
@@ -69,7 +85,22 @@ $("#searchBtn").on("click", function(event) {
     console.log(uvIndex)
     localStorage.setItem("uvScore", uvIndex)
     $("#uvIndex").empty().append(uvIndex);
+    
+    // Must be inside the delayed function
+    var uvScore = localStorage.getItem("uvScore");
+    function uvColor() {
+        if (uvScore <= 2){
+            $("#uvIndex").addClass("low").removeClass("moderate high severe");
+        } else if(uvScore > 2 && uvScore <= 5.99 ) {
+            $("#uvIndex").addClass("moderate").removeClass("low high severe");
+        } else if(uvScore >= 6 && uvScore <= 6.99 ) {
+            $("#uvIndex").addClass("high").removeClass("low moderate severe");
+        } else {
+            $("#uvIndex").addClass("severe").removeClass("low moderate high");
+        }
+    };
     uvColor();
+ 
 
     for (let i = 0; i < 5; i++) {
         
@@ -81,7 +112,8 @@ $("#searchBtn").on("click", function(event) {
         $(`#Temp${i}`).empty().append(temp + String.fromCharCode(176) + "F");
         $(`#Humidity${i}`).empty().append(humidity + String.fromCharCode(37));
     }
-});
+
+})};
 });
 
 let clearBtn = document.getElementById("clearBtn");
